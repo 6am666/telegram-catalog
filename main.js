a9e3798a07fa78c722805a6826d0f0743af66cf8
6am666 <yeba1225@gmail.com>
2026-01-15 03:09:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
-tg.MainButton.setParams({ color: '#000000', textColor: '#FFFFFF' });
+tg.MainButton.setParams({ color: '#FFFFFF', textColor: '#000000' });
 
 let cart = [];
 let inCartScreen = false;
@@ -8,7 +8,7 @@ let inCartScreen = false;
 const products = [
   { id: 1, name: "Колье Pierced Chain", price: 2500, image: "https://i.pinimg.com/736x/37/0b/db/370bdb870346b42b1000610195261f62.jpg" },
   { id: 2, name: "Колье Starry Sky", price: 4500, image: "https://i.pinimg.com/736x/55/bf/ec/55bfecc3c2ceebf20752ff2802ff4e19.jpg" },
-  { id: 3, name: "Колье Gothic Thorns", price: 3600, image: "https://via.placeholder.com/300" },
+  { id: 3, name: "Колье Gothic Thorns", price: 3600, image: "https://i.pinimg.com/736x/c2/0d/26/c20d26fb9839c64d328f8989450f547b.jpg" },
   { id: 4, name: "Браслет Hearts", price: 4000, image: "https://i.pinimg.com/736x/d4/c5/4c/d4c54cd9c489d1e73d9e306545929b70.jpg" },
   { id: 5, name: "Обвес Lighter", price: 3600, image: "https://i.pinimg.com/736x/e8/cb/c2/e8cbc2287025b23930c20e030755a0b5.jpg" },
   { id: 6, name: "Обвес Star", price: 2000, image: "https://i.pinimg.com/736x/16/36/75/163675cf410dfc51ef97238bbbab1056.jpg" },
@@ -17,12 +17,13 @@ const products = [
 ];
 
 const containerEl = document.getElementById("products");
+const mainTitle = document.getElementById("mainTitle");
+const cartIcon = document.getElementById("cartIcon");
 
 /* РЕНДЕР КАТАЛОГА */
 function renderProducts(list = products) {
   inCartScreen = false;
   document.body.style.backgroundColor = '#1e1e1e';
-  tg.MainButton.setParams({ color: '#000000', textColor: '#FFFFFF' });
   containerEl.innerHTML = "";
 
   list.forEach(p => {
@@ -43,7 +44,6 @@ function renderProducts(list = products) {
       <p>${p.price} ₽</p>
       <button class="add-btn">В корзину</button>
     `;
-
     card.prepend(img);
 
     card.querySelector(".add-btn").onclick = (e) => {
@@ -92,10 +92,9 @@ function updateMainButton() {
   }
 }
 
-/* Кнопка Telegram */
+/* Telegram кнопка */
 tg.MainButton.onClick(() => {
   if (inCartScreen) {
-    // Дальнейшие действия при оформлении заказа
     alert("Заказ оформлен!\n" + JSON.stringify(cart, null, 2));
     tg.sendData(JSON.stringify({ action: "order", items: cart }));
     cart = [];
@@ -133,8 +132,8 @@ function renderCart() {
         <p>${p.price} ₽</p>
         <button class="remove-btn">Удалить</button>
       `;
-
       card.prepend(img);
+
       card.querySelector(".remove-btn").onclick = () => removeFromCart(i);
       containerEl.appendChild(card);
     });
@@ -156,3 +155,9 @@ tg.CloudStorage.getItem("cart", (err, data) => {
   }
   renderProducts();
 });
+
+/* Переход на главную при клике по заголовку */
+mainTitle.onclick = () => renderProducts();
+
+/* Иконка корзины */
+cartIcon.onclick = () => renderCart();

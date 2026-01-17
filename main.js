26aa4bea2fa2f8be76ca710d178264b3b07c2396
6am666 <yeba1225@gmail.com>
2026-01-18 00:56:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ orderForm.innerHTML = `
 <input type="text" name="fullname" placeholder="Введите ФИО" required>
 
 <label>Адрес</label>
-<input type="text" id="addressInput" name="address" placeholder="Город, улица, дом, индекс" required>
+<input type="text" name="address" id="addressInput" placeholder="Город, улица, дом, индекс" required>
 
 <label>Доставка</label>
 <select name="delivery" id="deliverySelect" required>
@@ -142,7 +142,6 @@ orderForm.onsubmit = async e => {
 
   const fd = new FormData(orderForm);
   const productsList = cart.map(i => "• " + i.product.name + " x" + i.count).join("\n");
-
   const deliveryCost = (() => {
     switch (fd.get("delivery")) {
       case "СДЭК": return 450;
@@ -154,7 +153,7 @@ orderForm.onsubmit = async e => {
 
   const total = cart.reduce((s, i) => s + i.count * i.product.price, 0) + deliveryCost;
 
-  const orderData = {
+  const data = {
     fullname: fd.get("fullname"),
     phone: fd.get("phone"),
     telegram: fd.get("telegram"),
@@ -165,13 +164,10 @@ orderForm.onsubmit = async e => {
   };
 
   try {
-    // EmailJS
-    await emailjs.send("service_6drenuw", "template_90b82bq", orderData);
+    await emailjs.send("service_6drenuw", "template_90b82bq", data);
+    sendTelegramOrder(data);
 
-    // Telegram
-    sendTelegramOrder(orderData);
-
-    // ЮKassa
+    // ===== ЮKassa =====
     const res = await fetch("/api/create-payment", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -181,7 +177,6 @@ orderForm.onsubmit = async e => {
         return_url: window.location.origin + "?success=true"
       })
     });
-
     const json = await res.json();
 
     if (json.payment_url) {
@@ -196,7 +191,6 @@ orderForm.onsubmit = async e => {
       } else {
         window.open(json.payment_url, "_blank");
       }
-
     } else {
       alert("Ошибка создания оплаты");
     }
